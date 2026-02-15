62fa2bff1280cb2fbdc91fca4a1e0edc5278d28b
arnavanand2005 <arnavanand2005@gmail.com>
2026-02-15 20:12:16 +0530

    navbar  design finalised

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,26 @@
-import {Link} from 'react-router-dom';
-import './Navbar.css';
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className='navbar'>
-        <h1>🚦 ROAD SIGN DETECTION🚦</h1>
-        <div>
-            <Link to="/">PREDICTION</Link>
-            <Link to="/dataset">DATASET INFO</Link>
-        </div>
+    <nav className="navbar">
+      {/* Left: Logo / Title */}
+      <div className="navbar-left">
+        <span className="traffic-icon">🚦</span>
+        <h1 className="navbar-title">Road Sign Detection</h1>
+      </div>
+
+      {/* Right: Navigation Links */}
+      <div className="navbar-links">
+        <NavLink to="/" className="nav-link">
+          Prediction
+        </NavLink>
+
+        <NavLink to="/dataset" className="nav-link">
+          Dataset Info
+        </NavLink>
+      </div>
     </nav>
-  )
+  );
 }
